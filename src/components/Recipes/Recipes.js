256555d76470a1c9d03b94402c693efc0d599653
Manny Munoz <munoz.jeffrey95@yahoo.com>
2018-05-08 01:58:03 -0400

    Recipe add/close modal

--- src/components/Recipes/Recipes.js
+++ src/components/Recipes/Recipes.js
@@ -5,9 +5,9 @@ import './Recipes.css';
 export default class Recipes extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            recipes: []
-        }
+        this.state = { recipes: [], modalToggle: false };
+
+        this.handleToggleModal = this.handleToggleModal.bind(this);
     }
 
     componentWillMount() {
@@ -36,32 +36,39 @@ export default class Recipes extends Component {
 
     }
 
+    handleToggleModal(){
+        return this.setState(prevState => ({modalToggle: !prevState.modalToggle}));
+    }
+
     render() {
-        return (
-            <div>
-                <ul>
-                    {
-                        this.state.recipes.map((recipe) => (
-                        <li key={this.state.recipes.indexOf(recipe)}>
-                            {recipe.name} 
-                            <br/> 
-                            <ul>
-                                {
-                                    recipe.ingredients.map((ingredient) => (
-                                        <li key={recipe.ingredients.indexOf(ingredient)}> 
-                                            {
-                                                ingredient + " "
-                                            } 
-                                        </li>
-                                    ))
-                                }
-                            </ul>
-                        </li>))
-                    }
-                </ul>
-                
-                <button onClick={alert()}></button>
-            </div>
-        )
+        return <div>
+            <ul className="recipeList">
+              {this.state.recipes.map(recipe => (
+                <li key={this.state.recipes.indexOf(recipe)}>
+                  {recipe.name}
+                  <br />
+                  <ul>
+                    {recipe.ingredients.map(ingredient => (
+                      <li key={recipe.ingredients.indexOf(ingredient)}>
+                        {ingredient + " "}
+                      </li>
+                    ))}
+                  </ul>
+                </li>
+              ))}
+            </ul>
+
+            <ReactModal isOpen={this.state.modalToggle} ariaHideApp={false}>
+              <form>
+                <input type="text" placeholder="Recipe Name.." />
+                <input type="text" placeholder="Ingredient.." />
+                <input type="text" placeholder="Notes.." />
+                <button>Add recipe</button>
+              </form>
+              <button onClick={this.handleToggleModal}>X</button>
+            </ReactModal>
+
+            <button onClick={this.handleToggleModal}>Add</button>
+          </div>;
   }
 };
